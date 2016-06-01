f4356a3c0f21b90e59ea816084c2b16269ac4ac4
Joe Audette <joeaudette@users.noreply.github.com>
2016-06-01 14:52:42 -0400

    add examples localize views with shared resource

---
+++ src/WebLib/WebLibSharedResources.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebLib
+{
+    /// <summary>
+    /// the only purpose of this class is as a placeholder for shared resources
+    /// ie by asking for IStringLocalizer<WebLibSharedResources>
+    /// </summary>
+    public class WebLibSharedResources
+    {
+    }
+}
