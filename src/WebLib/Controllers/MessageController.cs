7babe6b24c2837aa55eb40a1ce4412994bb7df35
Joe Audette <joeaudette@users.noreply.github.com>
2016-11-28 10:51:25 -0500

    add data annotations example

---
+++ src/WebLib/Controllers/MessageController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using WebLib.ViewModels;
+
+namespace WebLib.Controllers
+{
+    public class MessageController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Message";
+
+            var model = new ContactMessageViewModel();
+
+            return View(model);
+        }
+    }
+}
---
+++ src/WebLib/ViewModels/ContactMessageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebLib.ViewModels
+{
+    public class ContactMessageViewModel
+    {
+        
+        [Required(ErrorMessage = "The Name field is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The Email field is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "The Message field is required.")]
+        public string Message { get; set; }
+    }
+}
